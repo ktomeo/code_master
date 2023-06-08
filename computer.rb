e206ad2c60da28e59fe43ee36c6f4d22ff001db1
Kai Tomeo <kaitomeo14@gmail.com>
2023-06-07 21:45:10 -0700

    added computer randomization of guesses

--- computer.rb
+++ computer.rb
@@ -5,4 +5,23 @@ class Computer
   def initialize
     @computer_code = []
   end
+
+  def generate_code
+    4.times do
+      code = rand(6)
+      if code == 0
+        @computer_code.push('red')
+      elsif code == 1
+        @computer_code.push('green')
+      elsif code == 2
+        @computer_code.push('yellow')
+      elsif code == 3
+        @computer_code.push('blue')
+      elsif code == 4
+        @computer_code.push('orange')
+      elsif code == 5
+        @computer_code.push('purple')
+      end
+    end
+  end
 end
